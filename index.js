594c08084c7bade842cd86d1e76ed83041f3416c
Madhu  Thoram <128285335+madhuthoram@users.noreply.github.com>
2023-11-05 10:30:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ document.addEventListener("keypress", function(event){
 function makesound(key){
     switch(key) {
         case "w":
-            var a = new Audio('sounds/crash.mp3');
+            var a = new Audio('crash.mp3');
             a.play();
             break;
         case "a":
-            var ko = new Audio('sounds/kick-bass.mp3');
+            var ko = new Audio('kick-bass.mp3');
             ko.play();
             break;
         case "s":
-            var snare = new Audio('sounds/snare.mp3');
+            var snare = new Audio('snare.mp3');
             snare.play();
             break;
         case "d":
-            var tom1 = new Audio('sounds/tom-1.mp3');
+            var tom1 = new Audio('tom-1.mp3');
             tom1.play();
             break;
         case "j":
-            var tom2 = new Audio('sounds/tom-2.mp3');
+            var tom2 = new Audio('tom-2.mp3');
             tom2.play();
             break;
         case "k":
-            var tom3 = new Audio('sounds/tom-3.mp3');
+            var tom3 = new Audio('tom-3.mp3');
             tom3.play();
             break;
         case "l":
-            var tom4 = new Audio('sounds/tom-4.mp3');
+            var tom4 = new Audio('tom-4.mp3');
             tom4.play();
             break;
         default:
